7f7d1b7de484a5e758dd0fcf0b83dd0939c284d1
SuetamId <mmcc.silveira@gmail.com>
2025-01-24 17:18:55 -0300

    feat: add search input component and integrate with champions list

--- src/app/pages/champions/champions.component.ts
+++ src/app/pages/champions/champions.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ChampionsHeaderComponent } from './components/champions-header/champions-header.component';
 import { ChampionsListComponent } from './components/champions-list/champions-list.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
 
 @Component({
   selector: 'app-champions',
   standalone: true,
-  imports: [ChampionsHeaderComponent, ChampionsListComponent],
+  imports: [ChampionsHeaderComponent, ChampionsListComponent, InputSearchComponent],
   templateUrl: './champions.component.html',
   styleUrl: './champions.component.scss'
 })
